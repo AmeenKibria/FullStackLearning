302de858c0f76f7ffea51126611eb1502e97cf6e
AmeenKibria <makibria@gmail.com>
2020-10-20 20:37:36 +0300

    exercise 1.6

--- index.js
+++ index.js
@@ -2,67 +2,31 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = (props) => (
-   
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-  )
-
-const Label = ({selected, votes}) => {
-    
-  return (
-        <>
-        <div>{anecdotes[selected]}</div>
-        <div>Has {votes[selected]} votes</div>
-        </>
-    )
-}
-const Statistics = (props) =>{
-  let i = props.votes.indexOf(Math.max(...props.votes));
-  return(
-    <div>
-      <h1>Most voted anecdote: </h1>
-      <div>{anecdotes[i]}</div>
-      <div>Has {props.votes[i]} votes</div>
-    </div>
-  )
-}
-
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
-  const [votes, setVote] = useState(Array(6).fill(0))
-
-  const setRandom = (props) => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
-
-  const giveVote = (selected) => {
-    const copy = [...votes]
-    copy[selected] +=1
-    setVote(copy)
-  }
-
-  return (
-    <div>
-      <Label selected= {selected} votes = {votes}/>
-      <p>
-      <Button handleClick={() => giveVote(selected)} text="Vote" />
-      <Button handleClick={() => setRandom(anecdotes)} text="Next anecdote" />
-      </p>
-      <Statistics votes = {votes} />
-    </div>
-  )
-}
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
+<button onClick = {props.handleClick}>
+  {props.text}
+</button>
 )
+
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutal, setNeutral] =useState (0)
+  const [bad, setBad] = useState (0)
+  
+  return (
+    <div>
+      <h1>Please give your feedback</h1>
+      <Button handleClick = {() => setGood(good + 1)} text="Good"/>
+      <Button handleClick = {() => setNeutral(neutal + 1)} text="Neutral"/>
+      <Button handleClick = {() => setBad(bad + 1)} text="Bad"/>
+      <h1>Statstics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutal}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+ 
+  ReactDOM.render(<App   />,
+  document.getElementById('root'))
